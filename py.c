7e26ee9718587fb30431f1e65cabaae51c91ad1f
bauuuu1021 <bboy010554@icloud.com>
2020-06-09 22:31:12 +0800

    Flush buffer

--- py.c
+++ py.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int main () {
     printf("Program Y\n");
 
     FILE *fp1 = fopen("/var/Y/legal.txt", "a");
-    fprintf(fp1, "success\n");
-    
+    if (fp1)
+        fprintf(fp1, "success\n");
+    fclose(fp1);
+
     FILE *fp2 = fopen("/var/failed.txt", "a");
-    fprintf(fp2, "failed\n");
+    if (fp2)
+        fprintf(fp2, "failed\n");
+    fclose(fp2);
 
     system("git clone https://github.com/jusbau/ProgramY");
 }
